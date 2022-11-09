a64e533dfefe6f833d37538df3b457408e91dc48
Caio Rodrigues <88396592+CaioRod41@users.noreply.github.com>
2022-11-09 20:37:11 -0300

    Add files via upload

---
+++ testex.py
@@ -0,0 +1,45 @@
+#Exercício 1
+
+#x = input()
+#x = int(x)
+#y = x * 100
+#z = y + x
+#print(z)
+
+#Exercício 2
+
+#s, x = input().split()
+#x = int(x)
+#z = x * 3.25
+#print("Foram "+str(x)+" pedaços de bolo de "+s+", então fica {:.2f} reais".format(z))
+
+#Exercício 3
+
+#x = input()
+#x = int(x)
+
+#result = ((x * 2) - 5) + pow(2 , x)
+#print(result)
+
+#Exercício 4
+
+#ono1, ono2, ono3 = input().split()
+#result = ono1 +  ono2 + ono2 + ono2 + ono3 + ono3
+#print(result)
+
+#Exercício 5
+
+#base  = input()
+#base = int(base)
+#altura = input()
+#altura = int(altura)
+#area = (base * altura)/2
+#print(f'{area:.2f}')
+
+#Exercício 6
+
+'''S = input()
+O = input()
+P = input()
+return = ( p + ( o + s ) )
+print(Return)'''
